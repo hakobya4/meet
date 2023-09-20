afeb8f173e7a7ab9884c8f931985ab75f354cb80
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-09-19 21:14:47 -0400

    removed mock data

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -1,23 +1,25 @@
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import EventDetails from '../components/Event';
-import mockData from '../mock-data';
+import { getEvents } from '../api';
 
 describe('<EventComponent /> component', () => {
   let EventComponent;
-  beforeEach(() => {
-    EventComponent = render(<EventDetails />);
+  let allEvents;
+  beforeEach(async() => {
+    allEvents = await getEvents();
+    EventComponent = render(<EventDetails event={allEvents[0]} />);
   });
 
 
   test('renders event summary', () => {
-    expect(EventComponent.queryByText(mockData[1].summary)).toBeInTheDocument();
+    expect(EventComponent.queryByText(allEvents[0].summary)).toBeInTheDocument();
   });
   test('renders event location', () => {
-    expect(EventComponent.queryByText(mockData[1].location)).toBeInTheDocument();
+    expect(EventComponent.queryByText(allEvents[0].location)).toBeInTheDocument();
   });
   test('renders event start time', () => {
-    expect(EventComponent.queryByText(mockData[1].created)).toBeInTheDocument();
+    expect(EventComponent.queryByText(allEvents[0].created)).toBeInTheDocument();
   });
   test('has the button to show details', () => {
     const button = EventComponent.queryByText('Show Details');
@@ -39,8 +41,10 @@ describe('<EventComponent /> component', () => {
     const user = userEvent.setup();
     const showButton = EventComponent.queryByText('Show Details');
     await user.click(showButton);
-    const eventKindON = EventComponent.queryByText('calendar#event');
-    expect(eventKindON).toBeInTheDocument();
+    const eventKindON = EventComponent.queryByText('Show Details');
+    const eventKindOFF = EventComponent.queryByText('Hide Details');
+    expect(eventKindON).not.toBeInTheDocument();
+    expect(eventKindOFF).toBeInTheDocument();
   });
 
   test('When user clicks hide details, details are hidden', async()=>{
@@ -49,7 +53,9 @@ describe('<EventComponent /> component', () => {
     await user.click(showButton);
     const hideButton = EventComponent.queryByText('Hide Details');
     await user.click(hideButton);
-    const eventKindOFF = EventComponent.queryByText('calendar#event');
+    const eventKindOFF = EventComponent.queryByText('Hide Details');
+    const eventKindON = EventComponent.queryByText('Show Details');
     expect(eventKindOFF).not.toBeInTheDocument();
+    expect(eventKindON).toBeInTheDocument();
   })
 });
