d2860082791b7809a756a129bbb3bd2342553cf1
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-09-19 20:18:53 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -86,3 +86,11 @@ const getToken = async (code) => {
 
   return access_token;
 };
+
+const checkToken = async (accessToken) => {
+  const response = await fetch(
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+  );
+  const result = await response.json();
+  return result;
+};
