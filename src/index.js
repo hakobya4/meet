64b26cec5a2b2daf78a8dc88586f46f577e8e5ca
hakobya4 <108638724+hakobya4@users.noreply.github.com>
2023-09-22 19:29:23 -0400

    web vitals

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('cab481e2d3da419e89e726990bf26c89').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
